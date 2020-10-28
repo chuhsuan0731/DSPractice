3b65abd68346340901d9d2a29d8fb331c6b784f5
chuhsuan0731 <71374913+chuhsuan0731@users.noreply.github.com>
2020-10-29 00:40:25 +0800

    Update hw.cpp

--- Array/hw.cpp
+++ Array/hw.cpp
@@ -1,50 +1,53 @@
-//TSENGCHUHSUAN 10/28
 //題目:利用一維靜態陣列去實作一個已知大小的二維陣列,
 //     1.get(i, j): 傳回二維陣列第 i 列，第 j 行的元素值,
 //     2.set(i, j, v): 設定二維陣列第 i 列，第 j 行的元素值為 v,
 //     3.輔助函數 dump() 將陣列內容印出，方便除錯
-
 #include <iostream>
-using namespace std; 
+#define ROWS 3
+#define LEN 4
+using namespace std;
 
-void dump()
+int main()
 {
-int i = 5;
-int j = 6;
-int m,n,v;
-int ARRAY[30];
+    int ARRAY[ROWS*LEN]={
+                            1,3,5,7,
+                            9,11,13,15,
+                            17,19,21,23
+                        };
+    int v;
     
-void get(int i,int j)
-{
-    	ARRAY[m*j+i];
-}
+    cout<<"傳回二維陣列第 i 列，第 j 行的元素值\n";
+	for(int i = 0; i <3; i++)
+	{
+		for(int j = 0; j < 4; j++)
+		{
+			cout<<"  get("<<i<<")("<<j<<")="<<ARRAY[j+i*4]<<'\t';	
+		}
+		cout<<endl;
+	}
 	
-set(int i,int j,int v)
-{
-	ARRAY[m*j+i]=v;
-}
-int ARRAY[30] = 
-{
-	{1, 2, 3,4, 5, 6},
-        {7,8,9,10,11,12},                
-	{13,14,15,16,17,18},                 
-	{19,20,21,22,23,24},
-	{25,26,27,28,29,30}
- };		
-for(int m = 0; m < 5; m++)
-{
-	for(int n = 0; n < 6; n++)
+	
+	cout<<"設定二維陣列第 i 列，第 j 行的元素值為 v\n";	
+	for(int i = 0; i <3; i++)
 	{
-		ARRAY[m*n] = (m + 1) * (n+ 1);
+		for(int j = 0; j < 4; j++)
+		{
+			v=ARRAY[j+i*4];
+			cout<<"  set("<<i<<")("<<j<<")"<<"("<<v<<")"<<'\t';	
+		}
+		cout<<endl;
 	}
+	
+	
+	cout<<"將陣列內容印出，方便除錯\n";
+	for(int i = 0; i <3; i++)
+	{
+		for(int j = 0; j < 4; j++)
+		{
+			cout<<ARRAY[j+i*4]<<'\t';	
+		}
+		cout<<endl;
+	}
+	
+    return 0;
 } 
-for(int m = 0; m < 5; m++)
-{
-	for(int n = 0; n < 6; n++)
-	{
-		cout << ARRAY[m*n] << "\t";
-	}
-	cout << endl;
-	}
-return 0;	
-}
