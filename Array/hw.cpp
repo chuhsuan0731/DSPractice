e8ac05806b66f547f145c01923a278923cae5b28
chuhsuan0731 <71374913+chuhsuan0731@users.noreply.github.com>
2020-10-28 15:11:20 +0800

    Create hw.cpp

---
+++ Array/hw.cpp
@@ -0,0 +1,39 @@
+//TSENGCHUHSUAN 10/28
+
+#include <iostream>
+using namespace std; 
+
+int main()
+{
+	int i = 5;
+    int j = 6;
+    int m,n,v;
+    int ARRAY[30];
+    
+    void get(int i,int j)
+    {
+    	ARRAY[m*j+i];
+	}
+	
+	void set(int i,int j,int v)
+	{
+		ARRAY[m*j+i]=v;
+	}
+		
+	for(int m = 0; m < i; m++)
+	{
+		for(int n = 0; n < j; n++)
+		{
+			ARRAY[m*n] = (m + 1) * (n+ 1);
+		}
+	} 
+	for(int m = 0; m < i; m++)
+	{
+		for(int n = 0; n < j; n++)
+		{
+			cout << ARRAY[m*n] << "\t";
+		}
+		cout << endl;
+	}
+	return 0;	
+}
